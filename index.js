2b31ae18fa97bd3b236f66a63734ed781452d6c4
Scott Hoisington <hoyzington@gmail.com>
2020-09-21 04:44:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,4 +61,6 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
+const { song_2, song_4 } = k_muppet.k_album.the_muppet_movie
+
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
