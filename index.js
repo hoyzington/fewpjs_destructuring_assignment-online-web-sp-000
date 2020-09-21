e2357c7b859ff1eaba6e076f5e4f22911c0ffcb7
Scott Hoisington <hoyzington@gmail.com>
2020-09-21 04:41:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,13 +21,15 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 // 4. Use destructuring to assign appropriate variables using the color names.
 
-const
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-
+const [r, o, y, g, b, , v] = colors
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+
+const [, , , , , indg, ] = colors
 
 
 // Objects
@@ -39,6 +41,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
+const { name, color, song, job, partner } = muppet
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
